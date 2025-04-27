c247f96c8386409a6e8d99379b3157a2ca23bdd8
marcovps <marcosmvp96@gmail.com>
2025-04-26 22:04:43 -0300

    ORM e Querys no Django

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here.
+def cars_view(request):
+
+    cars = Car.objects.all().order_by('brand')
+    search = request.GET.get('search')
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
+    
+
+    return render(
+        request, 
+        'cars.html', 
+        {'cars': cars})
