73fda532c66c3df680b87bcdc5cee84303b97d1c
marcovps <marcosmvp96@gmail.com>
2025-04-28 21:26:18 -0300

    Concluindo Forms no django

--- cars/forms.py
+++ cars/forms.py
@@ -1,12 +1,20 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Car
+  
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
-
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+    
+    def clean_value(self):
+        value  = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value','Valor minimo do carro deve ser de R$20.000! ')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year','Não é possível cadastrar carros fabricados antes de 1975')
+        return factory_year
